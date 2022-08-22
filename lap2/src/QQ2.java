0d060fa956686eedf7e26f6a2b9b83a054e37a1f
EMADIBRAHIMAMER <111526906+EMADIBRAHIMAMER@users.noreply.github.com>
2022-08-22 19:34:59 +0300

    Add files via upload

---
+++ lap2/src/QQ2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class QQ2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        double r,Area;
+        System.out.println("enter the number:");
+        r=in.nextFloat();
+        double p=3.1459;
+        System.out.println("perimeter="+2.0*(p*r));
+        System.out.println("area="+p*(r*r));
+}}
---
+++ lap2/src/QQ3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QQ3 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the number 1:");
+        int a= in.nextInt();
+        System.out.println("enter the number 1:");
+        int b= in.nextInt();
+        System.out.println("enter the number 1:");
+        int c= in.nextInt();
+        System.out.println("evareg="+(a+b+c)/3);
+
+    }
+}
